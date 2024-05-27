6dc81ca630916c29f53b42e3a8310e649ba9bf34
fcbyk <731240932@qq.com>
2024-05-28 04:33:50 +0800

    feat: 实现访问码功能

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,8 +1,8 @@
 // 主题入口文件创建的位置 .vitepress/theme/index.ts
 // 主题入口文件需要将主题（一个对象）作为默认导出
 
-import { h } from 'vue' // 导入h函数，用于创建虚拟节点
-import { useData } from 'vitepress' // useData函数是vp中用于获取页面数据的钩子函数
+import { h,ref} from 'vue' // 导入h函数，用于创建虚拟节点
+import { useData,useRouter} from 'vitepress' // useData函数是vp中用于获取页面数据的钩子函数
 import Theme from 'vitepress/theme' // 这里较模糊，官方文档的Theme是一个接口
 import "../style/layout.scss" // 导入样式文件
 import { DefaultTheme } from 'vitepress'
@@ -18,6 +18,20 @@ export default Object.assign({}, Theme, {
 
     // 获取页面的frontmatter
     const { frontmatter } = useData()
+
+    const routerInstance = useRouter()
+
+    console.log(routerInstance.route)
+
+    routerInstance.onBeforeRouteChange = (to) => {
+      if (to === '/resume') {
+        if(sessionStorage.getItem('isAuthenticated') == 'true') return true
+        
+        routerInstance.go('/resume-password')
+        return false
+      }
+
+    }
 
     // 如果页面frontmatter存在layoutClass属性，添加自定义class
     if (frontmatter.value?.layoutClass) {
--- docs/resume.md
+++ docs/resume.md
@@ -2,6 +2,17 @@
 layout: doc
 ---
 
+<script setup>  
+import { onBeforeMount} from 'vue'
+import { useRouter} from 'vitepress'
+
+onBeforeMount(() => {
+  const routerInstance = useRouter()
+  if(sessionStorage.getItem('isAuthenticated') != 'true') routerInstance.go('/resume-password')
+});
+
+</script>
+
 # 关于作者
 
 ## 等待编辑...
